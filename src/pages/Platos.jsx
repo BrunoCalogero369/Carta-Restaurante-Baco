5540df429e277dd0209a7d08f277ba19f5083d1e
Bruno Calogero <143133624+BrunoCalogero369@users.noreply.github.com>
2024-02-08 15:30:20 -0300

    Update Platos.jsx

--- src/pages/Platos.jsx
+++ src/pages/Platos.jsx
@@ -1,32 +1,8 @@
 import MenuSection from '../components/MenuSection';
-
-const platosData = [
-  {
-    imgSrc: 'LomoAltoBolixe.jpg',
-    alt: 'Ojo de Bife',
-    title: 'Ojo de Bife',
-    description: '390gr de Carne, punto de cocción a elección.',
-    price: '$3.900',   
-  },
-  {
-    imgSrc: 'sorrentinos-rellenos-de-jamon-y-queso-con-salsa-marinara.jpg',
-    alt: 'Sorrentinos de Jamon y Queso',
-    title: 'Sorrentinos de Jamon y Queso',
-    description: 'Plato con 6 unidades y salsa a elección.',
-    price: '$2.900',   
-  },
-  {
-    imgSrc: 'milanesa-napo.jpg',
-    alt: 'Milanesa Napolitana',
-    title: 'Milanesa Napolitana',
-    description: 'Plato de Milanesa de carne Napolitana.',
-    price: '$2.600',   
-  },
-
-];
+import productData from '../data/productData';
 
 function Platos() {
-  return <MenuSection title="Platos Principales" products={platosData} />;
+  return <MenuSection title="Platos Principales" products={productData} />;
 }
 
 export default Platos;
